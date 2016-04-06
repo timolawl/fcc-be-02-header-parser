b13d08d3f2cdfe8114175fbc9e6429c2cc5050cb
Timolawl <timolawl@users.noreply.github.com>
2016-04-06 16:39:22 -0700

    Add code to test functionality on heroku

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 'use strict';
 
 var express = require('express'),
-    favicon = require('serve-favicon'),
+    os = require('os');
+  //  favicon = require('serve-favicon');
   //  routes = require('./app/routes/index.js');
+
+function getSoftware() {
+    var osType;
+    osType = os.type();
+    if (osType === 'Darwin') osType = 'OS X';
+    else if (osType === 'Windows_NT') osType = 'Windows';
+
+    return osType;
+}
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(favicon(process.cwd() + '/public/images/favicon.ico'));
+app.get('/', function(req, res) {
+    res.send();
+});
 
+//app.enable('trust proxy');
 
 app.get('/api/whoami', function(req, res) {
-    var ip = req.headers['x-forwarded-for'] ||
+    var ip = req.headers['X-Forwarded-For'] ||
+        req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
-    var language = req.headers['accept-language'];
-    var os = process.platform;
+    var language = req.headers['accept-language'].match(/^\S+(?=,)/)[0];
 
-    res.json({ ipaddress: ip, language: language, software: os });
+    res.json({ ipaddress: ip, language: language, software: getSoftware(), architecture: os.arch() });
 });
+
 
 
 app.listen(app.get('port'), function() {
