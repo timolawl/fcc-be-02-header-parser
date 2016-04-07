a1a654a2b76a5d1c9dcf19258e727049a1df5d21
Timolawl <timolawl@users.noreply.github.com>
2016-04-06 17:12:24 -0700

    Fix return values

--- server.js
+++ server.js
@@ -2,17 +2,6 @@
 
 var express = require('express'),
     os = require('os');
-  //  favicon = require('serve-favicon');
-  //  routes = require('./app/routes/index.js');
-
-function getSoftware() {
-    var osType;
-    osType = os.type();
-    if (osType === 'Darwin') osType = 'OS X';
-    else if (osType === 'Windows_NT') osType = 'Windows';
-
-    return osType;
-}
 
 var app = express();
 
@@ -22,17 +11,17 @@ app.get('/', function(req, res) {
     res.send();
 });
 
-//app.enable('trust proxy');
-
 app.get('/api/whoami', function(req, res) {
+    console.log(req.headers['user-agent']);
     var ip = req.headers['X-Forwarded-For'] ||
         req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress;
     var language = req.headers['accept-language'].match(/^\S+(?=,)/)[0];
+    var software = req.headers['user-agent'].match(/\((.+?)(?=;)/).slice(1)[0];
 
-    res.json({ ipaddress: ip, language: language, software: getSoftware(), architecture: os.arch() });
+    res.json({ ipaddress: ip, language: language, software: software, architecture: os.arch() });
 });
 
 
